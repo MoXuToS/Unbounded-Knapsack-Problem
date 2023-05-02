5db0ea6147f29ee49c2755879b3ae7884a8daff8
Danila <68433423+MoXuToS@users.noreply.github.com>
2023-05-02 14:37:37 +0300

    Update Unbounded Knapsack Problem.cpp

--- Unbounded Knapsack Problem.cpp
+++ Unbounded Knapsack Problem.cpp
@@ -1,11 +1,3 @@
-//===========================================================
-// A tourist, going on a hike, buys products in indivisible
-// packages of known weight Cj and caloric content aj, j = 1..n.
-// The number of products of each type can be bought no more than dj
-// packages, j = 1..n.
-// Define a plan so that the calorie content is not lower than
-// To kilocalories, and the total weight was minimal
-//===========================================================
 #include<iostream>
 #include<string>
 #include<vector>
